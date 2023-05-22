d206ec9833618251a87d8baedbe88ee7c8ec9829
makaylamolina <121884929+makaylamolina@users.noreply.github.com>
2023-05-22 15:13:27 -0400

    made morgan add log to log.txt

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express'),
+  morgan = require('morgan'),
+  fs = require('fs'), // import built in node modules fs and path 
+  path = require('path');
+
+const app = express();
+// create a write stream (in append mode)
+// a ‘log.txt’ file is created in root directory
+const accessLogStream = fs.createWriteStream(path.join('log.txt'), {flags: 'a'})
+
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my app!');
+});
+
+app.get('/secreturl', (req, res) => {
+  res.send('This is a secret url with super top-secret content.');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
