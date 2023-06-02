b10437a2e82812d532fc4f682820cf7e316ed76b
makaylamolina <121884929+makaylamolina@users.noreply.github.com>
2023-06-02 15:34:44 -0400

    updated index

--- index.js
+++ index.js
@@ -159,13 +159,13 @@ app.get('/users/:Username', (req, res) => {
 // READ Get all movies
 app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
-  .then((movies) => {
-    res.status(201).json(movies);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send('Error: ' + err);
-  });
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
 // READ Get a movie by title
