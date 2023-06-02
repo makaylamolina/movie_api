292c775c351b7c5abdc51feadaf03068504d1bee
makaylamolina <121884929+makaylamolina@users.noreply.github.com>
2023-06-02 15:31:08 -0400

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,5 +1,5 @@
 const passport = require('passport'),
-  LocalStrategy = require('passport-local'),
+  LocalStrategy = require('passport-local').Strategy,
   Models = require('./models.js'),
   passportJWT = require('passport-jwt');
 
@@ -8,35 +8,35 @@ let Users = Models.User,
   ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(new LocalStrategy({
-    usernameField: 'Username',
-    passwordField: 'Password'
-  }, (username, password, callback) => {
-    console.log(username + '  ' + password);
-    Users.findOne({ Username: username }, (error, user) => {
-        if (error) {
-            console.log(error);
-            return callback(error);
-        }
+  usernameField: 'Username',
+  passwordField: 'Password'
+}, (username, password, callback) => {
+  console.log(username + '  ' + password);
+  Users.findOne({ Username: username }, (error, user) => {
+    if (error) {
+      console.log(error);
+      return callback(error);
+    }
 
-        if (!user) {
-            console.log('incorrect username');
-            return callback(null, false, {message: 'Incorrect username or password.'});
-        }
+    if (!user) {
+      console.log('incorrect username');
+      return callback(null, false, {message: 'Incorrect username or password.'});
+    }
 
-        console.log('finished');
-        return callback(null, user);
+    console.log('finished');
+    return callback(null, user);
+  });
+}));
+
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'your_jwt_secret'
+}, (jwtPayload, callback) => {
+  return Users.findById(jwtPayload._id)
+    .then((user) => {
+      return callback(null, user);
+    })
+    .catch((error) => {
+      return callback(error)
     });
-  }));
-
-  passport.use(new JWTStrategy({
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'your_jwt_secret'
-  }, (jwtPayload, callback) => {
-    return serializeUser.findById(jwtPayload._id)
-      .then((user) => {
-        return callback(null, user);
-      })
-      .catch((error) => {
-        return callback(error)
-      });
-  }));
+}));
