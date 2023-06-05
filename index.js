4165f7096c770dcec889231ba946951bebae71a4
makaylamolina <121884929+makaylamolina@users.noreply.github.com>
2023-06-04 20:39:08 -0400

    update mongoose.connect

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const { check, validationResult } = require('express-validator');
 const Movies = Models.Movie;
 const Users = Models.User
 
-mongoose.connect('mongodb://127.0.0.1:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://127.0.0.1:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
